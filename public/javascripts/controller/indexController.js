878743b7bc3e057eeee750694dfd55a4d5dedc51
Barış Özdemir <astute.developer@gmail.com>
2020-04-18 02:03:13 +0300

    User information control

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,10 +1,24 @@
 app.controller("indexController", ["$scope", "indexFactory", ($scope, indexFactory) => {
-    indexFactory.connectSocket("http://localhost:3000", {
-        reconnectionAttempts : 3,
-        reconnectionDelay : 500
-    }).then((socket) => {
-        console.log("Bağlantı Gerçekleşti!", socket);
-    }).catch((err) => {
-        console.log(err);
-    });
+    
+    $scope.init = () => {
+        const username = prompt("Lütfen Kullanıcı Adınızı Giriniz");
+
+        if(username){
+            initSocket(username);
+        } else {
+            return false;
+        }
+    };
+    
+    function initSocket (username)  {
+        indexFactory.connectSocket("http://localhost:3000", {
+            reconnectionAttempts : 3,
+            reconnectionDelay : 500
+        }).then((socket) => {
+            socket.emit("newUser", { username } )
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
+    
 }]);
